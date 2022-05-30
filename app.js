25b4fb2231c54c557315cad11f3f3280d326cf97
Jorge-Ortiz-M <jorge.ortiz@icalialabs.com>
2022-05-30 12:39:30 -0500

    Adding items to our to-do-list. We use an array and EJS within the html file. Remember, in order to write emmbeded js inside your html files, the extension must be change as .ejs and, you have to add some configuration inside of your package json like: npm install ejs, in order to start using this library.

--- app.js
+++ app.js
@@ -3,17 +3,39 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 4000;
 
+// -------- DATE section. -----------
+
+const options = {weekday: "long", day: "numeric", month: "long"};
+const today = new Date();
+const day = today.toLocaleDateString("en-US", options);
+console.log(day);
+
+// ---------- ITEMS section -----------
+
+let listItems = [];
+let item = '';
+
+// ---------------- ends -------------------
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 // -------------------- GET ------------------
 
 app.get("/", (req, res) => {
-    res.render('index', {foo: 3354})
-})
+    res.render('index', {currentDate: day, items: listItems});
+});
+
+// -------------------- POST ------------------
+
+app.post("/", (req, res) => {
+    item  = req.body.item;
+    listItems.push(item);
+    res.redirect("/");
+});
 
 // -------------------- PORTS ------------------
 
 app.listen(port, () => {
     console.log("Listening on port 4000");
-})
+});
