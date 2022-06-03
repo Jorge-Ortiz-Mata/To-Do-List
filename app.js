930f3d11b8db1361c70e45d5b52ddf50a9f6b6f1
Jorge-Ortiz-M <jorge.ortiz@icalialabs.com>
2022-06-03 17:56:38 -0500

    Adding dinamic routes. Users can create lists dinamically by typping the name of the list after the URL.

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 4000;
+// -------------------------------------------
 // -------- DATE section. -----------
 const day = require(__dirname + "/date.js");
+// -------------------------------------------
 // ---------------- APP Configuration -------------------
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 mongoose.connect("mongodb://localhost:27017/toDoList", { useNewUrlParser: true });
+// -------------------------------------------
 // ---------------- Create SCHEMAS -----------------
 const itemSchema = new mongoose.Schema({
     name:{
@@ -21,8 +24,16 @@ const itemSchema = new mongoose.Schema({
         required: true
     }
 });
+
+const listSchema = new mongoose.Schema({
+    name: String,
+    items: [itemSchema]
+});
+// -------------------------------------------
 // ---------------- Create MODELS ----------------
-const Item = mongoose.model("Item", itemSchema);
+const Item = mongoose.model("Item", itemSchema);    // It creates a collection with the itemSchema.
+const List = mongoose.model("List", listSchema);
+// -------------------------------------------
 // -------------------- GET ------------------
 app.get("/", (req, res) => {
     Item.find({}, function(err, items){
@@ -33,6 +44,25 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
     res.render('pages/about');
 });
+
+app.get("/:page", (req, res) => {
+    List.find({}, function(err, lists){
+        const page = req.params.page;
+        List.findOne({name: page}, function (err, currentList) {
+            if(currentList === null){
+                const newList = new List({
+                    name: page,
+                    items: []
+                });
+                newList.save();
+                res.redirect("/" + page);
+            } else {
+                res.render('pages/list', {currentDate: day.getDate(), list: currentList});
+            }
+        });
+    });
+})
+// -------------------------------------------
 // -------------------- POST ------------------
 app.post("/items", (req, res) => {
     let itemName  = req.body.item;
@@ -55,8 +85,8 @@ app.post("/delete", (req, res) => {
         }
     });
 });
+// -------------------------------------------
 // -------------------- PORTS ------------------
-
 app.listen(process.env.PORT || port, () => {
     console.log("Listening on port 4000");
 });
